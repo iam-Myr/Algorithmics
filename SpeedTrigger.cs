06e19af8d4cbe22aa4ca67a4c398399c07b286ce
Myriam Kapon <56133361+iam-Myr@users.noreply.github.com>
2025-03-26 16:22:07 +0200

    Add files via upload

--- SpeedTrigger.cs
+++ SpeedTrigger.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* This script increases an object's running speed when it enters the collision area 
+ * and descreases it when it leaves the collision area.*/
+
 public class SpeedTrigger : MonoBehaviour
 {
     public float speedFactor = 2.5f;
 
-    //When collider is entered
+    //Runs when collider is entered
     public void OnTriggerEnter(Collider other)
     {
         //Increases run speed 
@@ -14,7 +17,7 @@ public class SpeedTrigger : MonoBehaviour
         Debug.Log("Speed increased!");
     }
 
-    // When collider is exited
+    // Runs when collider is exited
     public void OnTriggerExit(Collider other)
     {
         // Decrease run speed
